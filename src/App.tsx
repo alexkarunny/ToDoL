dbcc1bf3a4bd7f32094471f5db5f84fe57a1ea02
alexkarunny <smetan4ik1@gmail.com>
2022-05-16 22:56:38 +0300

    Lesson 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import './App.css';
+import {Todolist} from "./Todolist";
+
+export type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
 
 function App() {
+    const tasks1: TaskType[] = [
+        {id: 1, title: 'HTML', isDone: true},
+        {id: 2, title: 'CSS', isDone: false},
+        {id: 3, title: 'JS', isDone: true},
+    ]
+    const tasks2: TaskType[] = [
+        {id: 1, title: 'Pickwick', isDone: true},
+        {id: 2, title: 'Kopperfield', isDone: false},
+        {id: 3, title: 'Oliver', isDone: false},
+    ]
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <Todolist title={'Learn'} tasks={tasks1}/>
+            <Todolist title={'Read'} tasks={tasks2}/>
         </div>
     );
 }
